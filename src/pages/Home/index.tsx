d21ac4b196465db739ead111e977f9bd8095756e
Samanta Fluture <samantafluture@gmail.com>
2022-10-13 12:44:04 -0400

    create and style search container

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,19 @@
 import { Profile } from './Profile'
-import { HomeContainer } from './styles'
+import { HomeContainer, SearchContainer } from './styles'
 
 export function Home() {
 	return (
 		<HomeContainer>
 			<Profile />
+			<SearchContainer>
+				<div>
+					<h2>Publications</h2>
+					<span>6 publications</span>
+				</div>
+        <form>
+          <input type="search" placeholder='Search content' />
+        </form>
+			</SearchContainer>
 		</HomeContainer>
 	)
 }
